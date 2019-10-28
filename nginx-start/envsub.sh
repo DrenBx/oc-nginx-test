fa64f58508c4f57320a6b27c96ea62aab747536f
DrenBx <DrenBx@users.noreply.github.com>
2019-10-28 16:20:53 +0100

    Update envsub.sh

--- nginx-start/envsub.sh
+++ nginx-start/envsub.sh
@@ -1,1 +1,2 @@
 envsubst '${SERVICE_URI}' < /opt/app-root/etc/nginx.d/upstreams.conf > /opt/app-root/etc/nginx.d/upstreams.conf
+envsubst '${SERVICE_URL}' < /opt/app-root/etc/nginx.default.d/nginx-proxy.conf > /opt/app-root/etc/nginx.default.d/nginx-proxy.conf

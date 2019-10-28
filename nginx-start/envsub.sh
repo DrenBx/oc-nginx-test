e7392befde4ca279109f03ec8330dce48ecd0e44
DrenBx <DrenBx@users.noreply.github.com>
2019-10-28 15:53:36 +0100

    Update and rename setenv.sh to envsub.sh

---
+++ nginx-start/envsub.sh
@@ -0,0 +1,1 @@
+envsubst '${SERVICE_URL} ${SERVICE_HOST} ${SERVICE_PORT}' < /opt/app-root/etc/nginx.d/upstreams.conf > /opt/app-root/etc/nginx.d/upstreams.conf
--- nginx-start/setenv.sh
+++
@@ -1,1 +0,0 @@
-envsubst '${SERVICE_HOST} ${SERVICE_PORT}' < /opt/app-root/etc/nginx.d/upstreams.conf > /opt/app-root/etc/nginx.d/upstreams.conf
